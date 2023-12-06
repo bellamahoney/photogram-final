8a85e8d03bbd41a5ceae6c63b689855c1abb42fb
bellamahoney <146144909+bellamahoney@users.noreply.github.com>
2023-12-06 23:34:29 +0000

    devise

---
+++ app/models/comment.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  author_id  :integer
+#  photo_id   :integer
+#
+class Comment < ApplicationRecord
+  validates(:author_id, presence: true)
+  belongs_to :photo, required: true, class_name: "Photo", foreign_key: "photo_id"
+  belongs_to :author, required: true, class_name: "User", foreign_key: "author_id"
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :likes
+  devise_for :follow_requests
+  devise_for :comments
+  devise_for :photos
+  devise_for :create_photos
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
